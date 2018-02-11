58e421f35815902b8f33ce831ff3234aa2bfedca
Scott Larribeau <slarribeau@gmail.com>
2018-02-11 00:20:58 -0800

    Complete stub implementation

--- src/test/ApacheHttpRestClent3Test.java
+++ src/test/ApacheHttpRestClent3Test.java
@@ -1,33 +1,28 @@
 package test;
 
-import junit.framework.Assert;
 import main.ApacheHttpRestClient3;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
+import java.io.InputStream;
+import static org.apache.commons.io.IOUtils.toInputStream;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ApacheHttpRestClent3Test extends Mockito {
-    String URI;
+    private String URI;
 
     @Before
     public void setUp() {
-       URI = "http://api.metro.net/agencies/lametro/routes/4/";
+        URI = "http://api.metro.net/agencies/lametro/routes/4/";
     }
 
     @Test
@@ -37,7 +32,7 @@ public class ApacheHttpRestClent3Test extends Mockito {
     }
 
     @Test
-    public void useInjectedMocksToReturnStatusTrue() throws ClientProtocolException, IOException {
+    public void useInjectedMocksToReturnStatusTrue() throws IOException {
         HttpClient httpClient = mock(HttpClient.class);
         HttpGet httpGet = mock(HttpGet.class);
         HttpResponse httpResponse = mock(HttpResponse.class);
@@ -52,7 +47,7 @@ public class ApacheHttpRestClent3Test extends Mockito {
     }
 
     @Test
-    public void useInjectedMocksToReturnStatusFalse() throws ClientProtocolException, IOException {
+    public void useInjectedMocksToReturnStatusFalse() throws IOException {
         HttpClient httpClient = mock(HttpClient.class);
         HttpGet httpGet = mock(HttpGet.class);
         HttpResponse httpResponse = mock(HttpResponse.class);
@@ -65,7 +60,7 @@ public class ApacheHttpRestClent3Test extends Mockito {
     }
 
     @Test
-    public void useInjectedRealObjectsToReturnRealStatus() throws ClientProtocolException, IOException {
+    public void useInjectedRealObjectsToReturnRealStatus() throws IOException {
         HttpClient httpClient = new DefaultHttpClient();
         HttpGet httpGetRequest = new HttpGet(URI);
         HttpResponse httpResponse = httpClient.execute(httpGetRequest);
@@ -76,13 +71,43 @@ public class ApacheHttpRestClent3Test extends Mockito {
     }
 
     @Test
-    public void useInjectedRealObjectsToPrintRealURI() throws ClientProtocolException, IOException {
+    public void useInjectedRealObjectsToPrintRealURI() throws IOException {
         HttpClient httpClient = new DefaultHttpClient();
         HttpGet httpGetRequest = new HttpGet(URI);
         HttpResponse httpResponse = httpClient.execute(httpGetRequest);
         StatusLine statusLine = httpResponse.getStatusLine();
 
         ApacheHttpRestClient3 client = new ApacheHttpRestClient3(URI);
+        assertTrue(client.printUriWithInjection());
+    }
+
+    @Test
+    public void useInjectedMockObjectsToPrintMockedlURI() throws IOException {
+        HttpClient httpClient = mock(HttpClient.class);
+        HttpGet httpGet = mock(HttpGet.class);
+        HttpResponse httpResponse = mock(HttpResponse.class);
+        StatusLine statusLine = mock(StatusLine.class);
+
+        HttpEntity entity = mock(HttpEntity.class);
+        InputStream inputStream = mock(InputStream.class);
+
+        InputStream stubInputStream =
+                toInputStream(  "{\n" +
+                                "   \"id\": \"4\",\n"  +
+                                "   \"fg_color\": \"#000000\",\n" +
+                                "   \"display_name\" \": 4 Downtown LA\",\n" +
+                                "   \"bg_color\": \"#f75d00\",\n" +
+                                "   \"24_hour\": \"true\"\n" +
+                                        "}",
+                                   "UTF-8");
+
+        when(entity.getContent()).thenReturn(stubInputStream);
+        when(httpResponse.getEntity()).thenReturn(entity);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(httpClient.execute(httpGet)).thenReturn(httpResponse);
+
+        ApacheHttpRestClient3 client = new ApacheHttpRestClient3(httpClient, httpGet, "");
         assertTrue(client.printUriWithInjection());
     }
 }
